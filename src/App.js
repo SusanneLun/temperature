4507d4dc12fa82330455f64cf4ebc82dbb40e298
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-05-07 16:24:04 +0100

    working app

--- src/App.js
+++ src/App.js
@@ -9,13 +9,45 @@ const API_KEY = "5c95c4fb3811e6af0f71b2a15dcd6333";
 
 class App extends Component {
 
+    state = {
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined
+    }
+
   getWeather =  async (e) => {
     e.preventDefault()
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
 
       const data = await api_call.json();
+      if (city && country) {
       console.log(data)
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
 
+      })
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter city and country"
+
+      })
+    }
     }
 
 render() {
@@ -24,7 +56,14 @@ render() {
 
     <Titles />
     <Form getWeather={this.getWeather}/>
-    <Weather />
+    <Weather
+    temperature={this.state.temperature}
+    city={this.state.city}
+    country={this.state.country}
+    humidity={this.state.humidity}
+    description={this.state.description}
+    error={this.state.error}
+    />
 
     </div>
   );
