6e14df7cd2d30dd572520d3cb2c1cef0f8efad7c
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-05-07 11:50:28 +0100

    app-created

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changing this text
         </a>
       </header>
     </div>
