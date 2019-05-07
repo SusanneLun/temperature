edb820feed5e8e5defe9cb06b342ab3b7a998b91
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-05-07 14:26:14 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Titles from "./Titles"
+import Form from "./Form"
+import Weather from "./Weather"
 
-function App() {
+class App extends Component {
+
+
+componentDidMount() {
+
+}
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Changing this text
-        </a>
-      </header>
+    <div>
+
+    <Titles />
+    <Form />
+    <Weather />
+
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class Form extends Component {
+
+render() {
+  return (
+    <div> 
+    Form Component
+    </div>
+  )
+}
+
+
+}
+
+export default Form;
