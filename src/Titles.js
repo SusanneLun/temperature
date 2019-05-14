44734899dbb0ab55a7968674c62d156f34bb8d9c
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-05-14 18:22:20 +0100

    more styling applied

--- src/Titles.js
+++ src/Titles.js
@@ -4,8 +4,9 @@ import React from "react";
 const Titles = () => (
 
       <div>
-        <h1>What's The Weather Like?</h1>
-        <p>Find out temperature and weather conditions</p>
+        <h1 className="title-container__title">What's The
+        Weather Like?</h1>
+        <p className="title-container__subtitle">Find out temperature and weather conditions</p>
       </div>
     )
 
