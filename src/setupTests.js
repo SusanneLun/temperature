873b070b15bb2ac42c16147f861a424d549391a4
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-06-09 16:58:57 +0100

    test preparations

---
+++ src/setupTests.js
@@ -0,0 +1,15 @@
+// react-testing-library renders your components to document.body,
+// this will ensure they're removed after each test.
+import 'react-testing-library/cleanup-after-each';
+// this adds jest-dom's custom assertions
+import 'jest-dom/extend-expect';
+
+
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
